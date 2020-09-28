720b737a6409fd5efb69d3a2cbab4f178e5a1f5f
nbarriav <nbarriav@uc.cl>
2020-09-28 09:02:30 -0300

    borrador

--- Sites/index.php
+++ Sites/index.php
@@ -63,16 +63,6 @@
 <br>
 
 
-  <h3 align="center">¿Quieres buscar todos los pokemones por tipo?</h3>
-
-  <?php
-  #Primero obtenemos todos los tipos de pokemones
-  require("config/conexion.php");
-  $result = $db -> prepare("SELECT DISTINCT tipo FROM pokemones;");
-  $result -> execute();
-  $dataCollected = $result -> fetchAll();
-  ?>
-
   
 <h3 align="center"> ¿Quieres ver el puerto que recibió más barcos en un mes y año?</h3>
 
@@ -89,24 +79,5 @@
 <br>
 <br>
 
-
-  <form align="center" action="consultas/consulta_tipo.php" method="post">
-    Seleccinar un tipo:
-    <select name="tipo">
-      <?php
-      #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
-      foreach ($dataCollected as $d) {
-        echo "<option value=$d[0]>$d[0]</option>";
-      }
-      ?>
-    </select>
-    <br><br>
-    <input type="submit" value="Buscar por tipo">
-  </form>
-
-  <br>
-  <br>
-  <br>
-  <br>
 </body>
 </html>
