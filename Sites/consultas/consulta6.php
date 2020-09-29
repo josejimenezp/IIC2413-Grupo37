7f90908c7f27d091617ca16fabbd85e96f32c196
nbarriav <nbarriav@uc.cl>
2020-09-28 21:29:15 -0300

    dict

--- Sites/consultas/consulta6.php
+++ Sites/consultas/consulta6.php
@@ -3,9 +3,24 @@
 <body>
 
 <?php
+
+  $meses = array(
+    "enero" => "01",
+    "febrero" => "02",
+    "marzo" => "03",
+    "abril" => "04",
+    "mayo" => "05",
+    "junio" => "06",
+    "julio" => "07",
+    "agosto" => "08",
+    "septiembre" => "09",
+    "octubre" => "10",
+    "noviembre" => "11",
+    "diciembre" => "12"
+  );
   require("../config/conexion.php");
 
-  $mes_elegido = $_POST["mes_elegido"];
+  $mes_elegido = $meses[strtolower($_POST["mes_elegido"])];
   $año_elegido = $_POST["año_elegido"];
 
   $consulta = "SELECT puertos.nombre, COUNT(permisos.patente) FROM permisos, puertos,
