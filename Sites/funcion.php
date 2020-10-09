7c40c3441990f61a5d88dae199b0ccf4f2e518a6
José Tomás Jiménez <jos.jimnez@uc.cl>
2020-10-09 18:54:34 -0300

    prueba joche3

--- Sites/funcion.php
+++ Sites/funcion.php
@@ -1,7 +1,7 @@
 <?php
 function funcion(){require("./config/conexion.php");
 $consulta = 'select nombre, edad, sexo, rut from personal, instalaciones where instalaciones.jefe_id = personal.rut and personal.iid = instalaciones.iid;';
-$prueba1 = 'select * from prueba;'
+$prueba1 = 'select * from prueba;';
 $res = $db -> prepare($prueba1);
 $res -> execute();
 $poto = $res -> fetchAll();
