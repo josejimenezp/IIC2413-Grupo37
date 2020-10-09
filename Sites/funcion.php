d88fd78809eb556e1523aaa3a52125b9d931002c
José Tomás Jiménez <jos.jimnez@uc.cl>
2020-10-09 18:55:35 -0300

    retorno

--- Sites/funcion.php
+++ Sites/funcion.php
@@ -1,12 +1,6 @@
 <?php
 function funcion(){require("./config/conexion.php");
 $consulta = 'select nombre, edad, sexo, rut from personal, instalaciones where instalaciones.jefe_id = personal.rut and personal.iid = instalaciones.iid;';
-$prueba1 = 'select * from prueba;';
-$res = $db -> prepare($prueba1);
-$res -> execute();
-$poto = $res -> fetchAll();
-$large = count($poto);
-echo $large;
 $resultado = $db -> prepare($consulta);
 $resultado -> execute();
 $jefes = $resultado -> fetchAll();
