89777d30a6b0ed573d195fc17af89fec52500b0b
nbarriav <nbarriav@uc.cl>
2020-10-20 17:07:41 -0300

    prueba boton

--- Sites/paginas/perfil.php
+++ Sites/paginas/perfil.php
@@ -56,7 +56,7 @@ if (isset($_SESSION['nombre'])) { // No se si esta bien
                     ?>
                 <br>
                 <form action="cambio_contrasena.php" method="post">    
-                <button class="login100-form-btn" type="submit">
+                <button class="myButton" type="submit">
 							Cambiar contraseña
 						</button>
                 </form>
