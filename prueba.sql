03e9da11b3474ff8436f3fb5fd3a55b77e22597b
nbarriav <nbarriav@uc.cl>
2020-10-21 17:00:47 -0300

    prueba final 1

--- prueba.sql
+++ prueba.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION capacidad_agotada(
 fecha_entrada date,
 fecha_salida date,
-puerto integer
+instalacion integer
 ) RETURNS table(fecha date) as $$
 DECLARE
 capacidad integer;
@@ -9,14 +9,16 @@ instalacion RECORD;
 fecha RECORD;
 id_instalacion integer;
 fecha_atraque integer;
+
 BEGIN
 CREATE TEMP TABLE IF NOT EXISTS aux(fecha date);
 DELETE FROM aux;
-FOR instalacion IN (select * from instalaciones where instalaciones.iid = puerto)
+
+FOR instalacion IN (select * from instalaciones where instalaciones.iid = instalacion)
 LOOP
 capacidad = instalacion.capacidad;
 id_instalacion = instalacion.iid;
-    FOR fecha IN (select permisos.fecha_atraque, count(permisos.fecha_atraque) from instalaciones, permisos WHERE instalaciones.iid = id_instalacion AND permisos.iid = instalaciones.iid GROUP BY permisos.fecha_atraque)
+    FOR fecha IN (select permisos.fecha_atraque, count(permisos.fecha_atraque) from instalaciones, permisos WHERE instalaciones.iid = id_instalacion AND permisos.iid = instalaciones.iid and fecha_atraque >= fecha_entrada and fecha_atraque <= fecha_salida GROUP BY permisos.fecha_atraque)
         LOOP
         if capacidad > fecha.count THEN
 		INSERT INTO aux VALUES(fecha.fecha_atraque);
