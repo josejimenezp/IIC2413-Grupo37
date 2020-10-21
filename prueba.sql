cafc73219a409522aa0688d79f19ad66b7a396b3
nbarriav <nbarriav@uc.cl>
2020-10-21 17:31:41 -0300

    asas

--- prueba.sql
+++ prueba.sql
@@ -20,7 +20,7 @@ capacidad = instalacion.capacidad;
 id_instalacion = instalacion.iid;
     FOR fecha IN (select permisos.fecha_atraque, count(permisos.fecha_atraque) from instalaciones, permisos WHERE instalaciones.iid = id_instalacion AND permisos.iid = instalaciones.iid and permisos.fecha_atraque >= fecha_entrada and permisos.fecha_atraque <= fecha_salida GROUP BY permisos.fecha_atraque)
         LOOP
-        if capacidad > fecha.count THEN
+        if capacidad <= fecha.count THEN
 		INSERT INTO aux VALUES(fecha.fecha_atraque);
 	END if;
 	END LOOP;
