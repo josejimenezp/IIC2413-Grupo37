b07dc7967c4a7fd2a9768ccb1a6a88967823a0cf
nbarriav <nbarriav@uc.cl>
2020-10-22 18:00:39 -0300

    asfsafa

--- Sites/paginas/capacidad_agotada.php
+++ Sites/paginas/capacidad_agotada.php
@@ -6,11 +6,15 @@ if (isset($_SESSION['nombre'])) {
     require("header.php");
 }
     require("../config/conexion.php");
-    $fecha_entrada = $_POST["fecha_entrada"];
-    $fecha_salida = $_POST["fecha_salida"];
+    $fecha_entrada = $_POST["fecha_entrada"]
+    $fecha_salida = $_POST["fecha_salida"]
 
+    $query = "SELECT capacidad_agotada('$fecha_entrada','$fecha_salida',1);"; // 
+    
+    $result = $db_puertos -> prepare($query);
+    $result -> execute();
+    $resultados = $result -> fetchAll();  // Resultados de buques
 
-    echo $fecha_entrada;
-    echo $fecha_salida;
+    echo $resultados
 
     ?>
