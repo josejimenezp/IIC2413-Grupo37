d7b34a7a0e6271d7e89ecad392508863b940ce97
nbarriav <nbarriav@uc.cl>
2020-10-22 18:21:28 -0300

    asdads

--- stored_btn1.sql
+++ stored_btn1.sql
@@ -27,13 +27,14 @@ CREATE OR REPLACE FUNCTION capacidad_agotada(
 fecha_entrada date,
 fecha_salida date,
 instalacion_in integer
-) RETURNS table(fecha date) as $$
+) RETURNS table(fecha date, porcentaje real) as $$
 DECLARE
 capacidad integer;
 instalacion RECORD;
 fecha RECORD;
 id_instalacion integer;
 fecha_atraque integer;
+porcentaje_capacidad real;
 
 BEGIN
 CREATE TEMP TABLE IF NOT EXISTS aux(fecha date);
@@ -46,7 +47,8 @@ id_instalacion = instalacion.iid;
     FOR fecha IN (select permisos.fecha_atraque, count(permisos.fecha_atraque) from instalaciones, permisos WHERE instalaciones.iid = id_instalacion AND permisos.iid = instalaciones.iid and permisos.fecha_atraque >= fecha_entrada and permisos.fecha_atraque <= fecha_salida GROUP BY permisos.fecha_atraque)
         LOOP
         if capacidad <= fecha.count THEN
-		INSERT INTO aux VALUES(fecha.fecha_atraque);
+		porcentaje_capacidad = fecha.count/capacidad;
+		INSERT INTO aux VALUES(fecha.fecha_atraque, porcentaje_capacidad);
 	END if;
 	END LOOP;
 RETURN QUERY SELECT * FROM (SELECT * FROM fechas(fecha_entrada,fecha_salida) EXCEPT (SELECT * FROM aux)) as foo ORDER BY foo.fecha;
