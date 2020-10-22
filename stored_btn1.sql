aa41cbe8cb3b26c9443e127f224e165b02e73a4d
nbarriav <nbarriav@uc.cl>
2020-10-22 19:39:28 -0300

    asdas

--- stored_btn1.sql
+++ stored_btn1.sql
@@ -1,19 +1,19 @@
 CREATE OR REPLACE FUNCTION fechas(
 fecha_entrada date,
 fecha_salida date
-) returns table(fecha date) as $$
+) returns table(fecha date, capacidad_porcentual real) as $$
 
 DECLARE
 fecha date;
 
 BEGIN
 
-CREATE TEMP TABLE IF NOT EXISTS fecha_table(fecha date); 
+CREATE TEMP TABLE IF NOT EXISTS fecha_table(fecha date, capacidad_porcentual real); 
 DELETE FROM fecha_table;
 fecha = fecha_entrada;
 WHILE fecha != fecha_salida + 1
 LOOP
-INSERT INTO fecha_table VALUES (fecha);
+INSERT INTO fecha_table VALUES (fecha,100);
 fecha = fecha + 1;
 END LOOP;
 RETURN QUERY SELECT * FROM fecha_table;
@@ -41,7 +41,6 @@ CREATE TEMP TABLE IF NOT EXISTS aux(fecha date, porcentaje_capacidad real);
 DELETE FROM aux;
 
 PERFORM fechas(fecha_entrada,fecha_salida);
-ALTER TABLE fecha_table ADD COLUMN porcentaje_ocupacion real;
 FOR instalacion IN (select instalaciones.iid, instalaciones.capacidad from instalaciones where instalaciones.iid = instalacion_in)
 LOOP
 capacidad = instalacion.capacidad;
@@ -49,10 +48,9 @@ id_instalacion = instalacion.iid;
     FOR fecha IN (select permisos.fecha_atraque, count(permisos.fecha_atraque) from instalaciones, permisos WHERE instalaciones.iid = id_instalacion AND permisos.iid = instalaciones.iid and permisos.fecha_atraque >= fecha_entrada and permisos.fecha_atraque <= fecha_salida GROUP BY permisos.fecha_atraque)
         LOOP
         if capacidad <= fecha.count THEN
-		porcentaje_capacidadvar = fecha.count/capacidad;
-		INSERT INTO aux VALUES(fecha.fecha_atraque, porcentaje_capacidadvar);
+		INSERT INTO aux VALUES(fecha.fecha_atraque, 0);
 	else
-		porcentaje_capacidadvar = 100;
+		porcentaje_capacidadvar = 100 - fecha.count/capacidad;
 	END if;
 	UPDATE fecha_table SET porcentaje_ocupacion = porcentaje_capacidadvar WHERE fecha_table.fecha = fecha.fecha_atraque;
 	END LOOP;
