f401fed3905dfbebfaf9e77f316c57efebb0710d
nbarriav <nbarriav@uc.cl>
2020-10-22 19:20:13 -0300

    asdasd

--- stored_btn1.sql
+++ stored_btn1.sql
@@ -54,8 +54,8 @@ id_instalacion = instalacion.iid;
 	else
 		porcentaje_capacidadvar = 100;
 	END if;
-	ALTER TABLE fechas_table ADD COLUMN porcentaje_ocupacion real;
-	UPDATE fechas_table SET porcentaje_ocupacion = porcentaje_capacidadvar WHERE fechas.fecha = fecha.fecha_atraque;
+	ALTER TABLE fecha_table ADD COLUMN porcentaje_ocupacion real;
+	UPDATE fecha_table SET porcentaje_ocupacion = porcentaje_capacidadvar WHERE fecha_table.fecha = fecha.fecha_atraque;
 	END LOOP;
 RETURN QUERY SELECT * FROM (SELECT * FROM fecha_table EXCEPT (SELECT aux.fecha FROM aux)) as foo ORDER BY foo.fecha;
 END LOOP;
