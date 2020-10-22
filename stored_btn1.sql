8b7062a6cb397a456d6a66d8935b90f79197d4e4
nbarriav <nbarriav@uc.cl>
2020-10-22 20:35:27 -0300

    assdsa

--- stored_btn1.sql
+++ stored_btn1.sql
@@ -51,7 +51,7 @@ id_instalacion = instalacion.iid;
 		porcentaje_capacidadvar = 0;
 		INSERT INTO aux VALUES(fecha.fecha_atraque, 0);
 	else
-		porcentaje_capacidadvar = 1/3;
+		porcentaje_capacidadvar = CAST( 1 as decimal) / cast(3 as decimal);
 		INSERT INTO aux VALUES(fecha.fecha_atraque, porcentaje_capacidadvar);
 		UPDATE fecha_table SET capacidad_porcentual = porcentaje_capacidadvar WHERE fecha_table.fechaxd = fecha.fecha_atraque;
 	END if;
