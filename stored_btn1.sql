3e3dd46f5fb836a499b2eb1cc0d7c18ae0a321ae
nbarriav <nbarriav@uc.cl>
2020-10-23 10:25:06 -0300

    asdasd

--- stored_btn1.sql
+++ stored_btn1.sql
@@ -13,7 +13,7 @@ DELETE FROM fecha_table;
 fecha = fecha_entrada;
 WHILE fecha != fecha_salida + 1
 LOOP
-INSERT INTO fecha_table VALUES (fecha,100);
+INSERT INTO fecha_table VALUES (fecha,0);
 fecha = fecha + 1;
 END LOOP;
 RETURN QUERY SELECT * FROM fecha_table;
@@ -27,7 +27,7 @@ CREATE OR REPLACE FUNCTION capacidad_agotada(
 fecha_entrada date,
 fecha_salida date,
 instalacion_in integer
-) RETURNS table(fecha date) as $$
+) RETURNS table(fecha date, porcentaje real) as $$
 DECLARE
 capacidad integer;
 instalacion RECORD;
@@ -48,10 +48,10 @@ id_instalacion = instalacion.iid;
     FOR fecha IN (select permisos.fecha_atraque, count(permisos.fecha_atraque) from instalaciones, permisos WHERE instalaciones.iid = id_instalacion AND permisos.iid = instalaciones.iid and permisos.fecha_atraque >= fecha_entrada and permisos.fecha_atraque <= fecha_salida GROUP BY permisos.fecha_atraque)
         LOOP
         if capacidad <= fecha.count THEN
-		porcentaje_capacidadvar = 0;
+		porcentaje_capacidadvar = 100;
 		INSERT INTO aux VALUES(fecha.fecha_atraque, 0);
 	else
-		porcentaje_capacidadvar = 100-100*(CAST( fecha.count as decimal) / cast(capacidad as decimal));
+		porcentaje_capacidadvar = 100*(CAST( fecha.count as decimal) / cast(capacidad as decimal));
 		UPDATE fecha_table SET capacidad_porcentual = porcentaje_capacidadvar WHERE fecha_table.fechaxd = fecha.fecha_atraque;
 	END if;
 	UPDATE fecha_table SET capacidad_porcentual = porcentaje_capacidadvar WHERE fecha_table.fechaxd = fecha.fecha_atraque;
