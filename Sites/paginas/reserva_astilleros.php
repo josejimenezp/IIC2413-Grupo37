2c574aa80fdbc7e568a06bcec3c318a8ba4b24da
nbarriav <nbarriav@uc.cl>
2020-10-26 12:42:57 -0300

    asdadsd

---
+++ Sites/paginas/reserva_astilleros.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+require("../config/conexion.php");
+$query_cantidad = "SELECT MAX(permisos.pid) FROM permisos;";
+
+$id_max = $db_puertos -> prepare($query_cantidad);
+$id_max -> execute();
+$id_max = $id_max -> fetchAll();
+
+$id_max = $id_max[0][0];
+
+$pid = $id_max + 1;
+$iid = $_GET['iid'];
+$patente = $_SESSION['patente'];
+$fecha_atraque = $_SESSION['fecha_astillero_entrada'];
+$fecha_salida = $_SESSION['fecha_astillero_salida'];
+
+$query_insert = "INSERT INTO permisos VALUES ($pid, $iid, '$patente', '$fecha_atraque');";
+$insert = $db_puertos -> prepare($query_insert);
+$insert -> execute();
+$insert = $insert -> fetchAll();
+
+$query_insert = "INSERT INTO permisos_astillero VALUES ($pid,'$fecha_salida');";
+$insert = $db_puertos -> prepare($query_insert);
+$insert -> execute();
+$insert = $insert -> fetchAll();
+echo "<script>alert('Reserva existosa');
+    window.location.href='vista_puertos.php'</script>";
+?>
