abee4e22112241a56f2b526949e63f44369a8ca5
José Tomás Jiménez <jos.jimnez@uc.cl>
2020-12-06 19:23:03 -0300

    hola

---
+++ Sites/paginas/busqueda_sospechoso.php
@@ -0,0 +1,64 @@
+<?php
+require("../config/conexion.php");
+@session_start();
+if (isset($_SESSION['username'])) {
+    require("header.php");
+} else {
+    header("location: ../index.php");
+}
+?>
+
+<?php>
+$id_usuario = $_POST['id'];
+$fecha_1 = $_POST['fecha1'];
+$fecha_2 = $_POST['fecha2'];
+$palabras_clave = $_POST['palabras_clave']
+
+
+$tipo = "SELECT tipo FROM usuarios WHERE usuarios.uid = $id_usuarios;"
+
+if ($tipo == 'Capitan') {
+    $obtener_rut = "SELECT n_pasaporte FROM usuarios WHERE usuarios.uid = $id_usuario;"
+    $result = $db_puertos -> prepare($obtener_rut);
+    $result -> execute();
+    $resultado = $result-> fetchAll();
+    
+    $pasaporte = $resultado[0][0]
+    $obtener_id = "SELECT pid FROM personas WHERE pasaporte = $pasaporte;"
+    $result = $db_buques -> prepare($obtener_id);
+    $result -> execute();
+    $resultado = $result-> fetchAll();
+
+    $id_nuevo = $resultado[0][0]
+    $obtener_barcos = "SELECT bid FROM buques WHERE buques.id_capitan = $id_nuevo;"
+    $result = $db_buques -> prepare($id_nuevo);
+    $result -> execute();
+    $resultado = $result-> fetchAll();
+
+    $id_buque = $resultado[0][0]
+    $id_puerto = "SELECT id_puerto FROM atraques WHERE atraques.buque = $buque AND fecha_atraque >= $fecha_1 AND fecha_atraque <= $fecha_2;"
+    $result = $db_buques -> prepare($id_puerto);
+    $result -> execute();
+    $resultado = $result-> fetchAll();
+
+    $puertos = $resultado[0][0]
+    echo $puerto
+};
+
+elif ($tipo == 'Jefe') {
+    $obtener_rut = "SELECT n_pasaporte FROM usuarios WHERE usuarios.uid = $id_usuario;"
+    $result = $db_puertos -> prepare($obtener_rut);
+    $result -> execute();
+    $resultado = $result-> fetchAll();
+    
+    $pasaporte = $resultado[0][0]
+    $es_jefe = "SELECT puertos.nombre FROM usuarios, instalaciones, puertos WHERE usuarios.n_pasaporte = instalaciones.jefe_id AND instalaciones.puid = puertos.puid AND usuarios.n_pasaporte = $pasaporte;"
+    $result = $db_puertos -> prepare($es_jefe);
+    $result -> execute();
+    $resultado = $result-> fetchAll();
+
+    $puerto = $resultado[0][0]
+    echo $puerto
+}
+
+?>
--- Sites/paginas/pdi.php
+++ Sites/paginas/pdi.php
@@ -7,12 +7,12 @@ if (isset($_SESSION['username'])) {
 }
 ?>
 
-<form action="cambio_contrasena.php" method="post">
+<form action="busqueda_sospechoso.php" method="post">
     <label for='id'>ID de usuarios:</label><br>    
     <input type='number' id='id' name='id'><br><br>
     <label for='date'>Fechas:</label><br>    
-    <input type='date' id='date' name='date'><bc>
-    <input type='date' id='date' name='date'>
+    <input type='date' id='date' name='fecha1'><bc>
+    <input type='date' id='date' name='fecha2'>
     <br><br>
     <label for='palabras_clave'>Palabras clave:</label><br>
     <input type='text' id='palabras_clave' name='palabras_clave'><br><br>
