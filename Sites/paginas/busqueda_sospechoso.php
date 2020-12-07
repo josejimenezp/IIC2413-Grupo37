40c3b1e1b97a66fe79e7e30d5872cd2511288057
nbarriav <nbarriav@uc.cl>
2020-12-07 16:34:26 -0300

    asdas

--- Sites/paginas/busqueda_sospechoso.php
+++ Sites/paginas/busqueda_sospechoso.php
@@ -50,11 +50,8 @@ $data = json_decode(trim($result), TRUE);
         <script> 
             var map = L.map('mapid').setView([-33.499572, -70.615472], 10);
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox.streets',
-            accessToken: 'your.mapbox.access.token'
-            }).addTo(mymap);
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(mymap);
             <?php foreach ($data as $punto) {
                 $long = $punto['long'];
                 $lat  = $punto['lat'];
