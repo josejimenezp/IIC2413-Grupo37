0ba9cdc0e77cf7560beb756b4233c4df5f67975b
nbarriav <nbarriav@uc.cl>
2020-12-07 15:03:14 -0300

    asa

--- Sites/paginas/busqueda_sospechoso.php
+++ Sites/paginas/busqueda_sospechoso.php
@@ -7,7 +7,9 @@ if (isset($_SESSION['username'])) {
     header("location: ../index.php");
 }
 ?>
-
+<script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js"
+    integrity="sha512-QVftwZFqvtRNi0ZyCtsznlKSWOStnDORoefr1enyq5mVL4tmKB3S/EnC3rRJcxCPavG10IcrVGSmPh6Qw5lwrg=="
+    crossorigin=""></script>
 <?php
 $curl = curl_init();
 $id_usuario = $_POST['id'];
