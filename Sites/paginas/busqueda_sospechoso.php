1a14357c3a386962925035d2b0aa68fe03b6dcb8
nbarriav <nbarriav@uc.cl>
2020-12-07 15:31:14 -0300

    sdd

--- Sites/paginas/busqueda_sospechoso.php
+++ Sites/paginas/busqueda_sospechoso.php
@@ -48,7 +48,7 @@ $data = json_decode(trim($result), TRUE);
 
         <script> 
             const data = <?php echo $data; ?>;
-            data;
+            echo "Hola";
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
