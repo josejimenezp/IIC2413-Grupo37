320259d83b98ae30985f450d120f28c5fe9de615
nbarriav <nbarriav@uc.cl>
2020-12-07 18:33:47 -0300

    adasdas

--- Sites/paginas/busqueda_sospechoso.php
+++ Sites/paginas/busqueda_sospechoso.php
@@ -48,8 +48,7 @@ if ($tipo == 'Capitan') {
     $resultado = $result-> fetchAll();
 
     foreach ($resultado as $resultado) {
-        echo $resultado;
-        echo "Hola";
+        echo $resultado[0];
         //$query_puerto = "SELECT latidud, longitud FROM coordenadas_puertos WHERE coordenadas_puertos.puerto = '$resultado';";
         //$result = $db_puertos -> prepare($query_puerto);
         //$result -> execute();
