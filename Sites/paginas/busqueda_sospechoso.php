b89473b8e508d974c4075a192489ffa8eaee0fb3
nbarriav <nbarriav@uc.cl>
2020-12-11 14:00:24 -0300

    asdd

--- Sites/paginas/busqueda_sospechoso.php
+++ Sites/paginas/busqueda_sospechoso.php
@@ -114,4 +114,4 @@ $context  = stream_context_create( $options );
 $result = file_get_contents('http://young-ocean-30844.herokuapp.com/text-search', false, $context );
 $result = json_decode($result, true);
 };
-echo $result
+echo $result;
